7bdf2c378af9b03010440843e90cbc3ad9f27b2c
Petros <morakos32@yahoo.gr>
2018-09-29 17:34:18 +0300

    Modified the CMakeLists.
    Added the lz77 class for the LZ77 lossless data compression algorithm.

---
+++ include/lz77.h
@@ -0,0 +1,22 @@
+#ifndef LZ77_H
+#define LZ77_H
+
+#include <string>
+
+class LZ77
+{
+public:
+
+    std::string payload;
+
+
+    LZ77(std::string _payload);
+    ~LZ77() = default;
+    LZ77()  = default;
+    LZ77(const LZ77& T)            = default;
+    LZ77& operator=(const LZ77& T) = default;
+    LZ77(LZ77&& T)                 = default;
+    LZ77& operator=(LZ77&& T)      = default;
+
+};
+#endif // LZ77_H
