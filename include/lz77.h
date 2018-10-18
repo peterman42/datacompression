89df3537524d8aa1706a6d0d1da42ca4e96e0d32
Petros <morakos32@yahoo.gr>
2018-10-18 23:27:39 +0300

    Added constructor and variables.

--- include/lz77.h
+++ include/lz77.h
@@ -1,26 +1,35 @@
 #ifndef LZ77_H
 #define LZ77_H
 
+#include <stdint.h>
 #include <string>
 #include <map>
+#include <list>
 
 class LZ77
 {
 public:
 
-    std::string payload;
     std::map<std::string,std::tuple<int,int,std::string>> dictionary;
 
     ///////////////////////////////////////////////////////////////////////////////
     void compress();
 
-    LZ77(std::string _payload);
+    LZ77( uint32_t _search_buffer_size
+        , uint32_t _lookahead_buffer_size
+        , std::string _payload);
     ~LZ77() = default;
     LZ77()  = default;
     LZ77(const LZ77& T)            = default;
     LZ77& operator=(const LZ77& T) = default;
     LZ77(LZ77&& T)                 = default;
     LZ77& operator=(LZ77&& T)      = default;
-
+private :
+    uint32_t search_buffer_size;
+    uint32_t lookahead_buffer_size;
+    std::string payload;
+    std::list tuples;
+    std::list search_buffer;
+    std::list lookahead_buffer;
 };
 #endif // LZ77_H
--- src/lz77.cpp
+++ src/lz77.cpp
@@ -1,9 +1,18 @@
 #include "lz77.h"
 #include <iostream>
 #include <tuple>
+#include <algorithm>
 
-LZ77::LZ77(std::string _payload)
-	: payload(_payload)
+LZ77::LZ77( uint32_t _search_buffer_size
+          , uint32_t _lookahead_buffer_size
+          , std::string _payload)
+    : search_buffer_size(_search_buffer_size)
+    , lookahead_buffer_size(_lookahead_buffer_size)
+    , payload(_payload)
+    , tuples()
+    , search_buffer()
+    , lookahead_buffer()
+
 {}
 
 
@@ -11,9 +20,15 @@ LZ77::LZ77(std::string _payload)
 void
 LZ77::compress()
 {
-    std::tuple<int,int,std::string> tuple = {0, 0, ""};
+    // {0,0,""} 1: position, 2: length, 3: next character
+    std::list buffer(payload.begin(), payload.end());
     for(char character : payload)
     {
+        std::find_if(search_buffer.begin(),search_buffer.end(),[](auto tuple)
+        {
+
+        });
+
         std::cout << "Character :: " << character << std::endl;
     }
 }
