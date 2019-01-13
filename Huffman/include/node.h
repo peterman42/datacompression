e623fe87136b102813a282a5abce836454c7e7d0
Petros <morakos32@yahoo.gr>
2019-01-13 19:55:34 +0200

    Added the node header file

---
+++ Huffman/include/node.h
@@ -0,0 +1,22 @@
+#ifndef NODE_H
+#define NODE_H
+#include <memory>
+
+namespace DataCompression { namespace Huffman {
+
+class Node
+{
+    std::shared_ptr<Node> right;
+    std::shared_ptr<Node> left;
+    uint32_t value;
+
+    ///////////////////////////////////////////////////////////////////////////////
+    ~Node() = default;
+    Node(const Node& T) = default;
+    Node& operator=(const Node& T) = default;
+    Node(Node&& T) = default;
+    Node& operator=(Node&& T) = default;
+
+};
+} }
+#endif
