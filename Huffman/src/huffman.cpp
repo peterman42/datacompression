70e38ae21023a6bfc831e70204fd667a1c11e58b
Petros <morakos32@yahoo.gr>
2019-01-13 22:44:18 +0200

    Development into the countCharacters function.

--- Huffman/src/huffman.cpp
+++ Huffman/src/huffman.cpp
@@ -1,4 +1,6 @@
 #include <huffman.h>
+#include <algorithm>
+
 namespace DataCompression { namespace Huffman {
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -11,4 +13,43 @@ void Huffman::decode(const std::string& text)
 {
 
 }
-void Huffman::encode(const std::string& text)
+
+///////////////////////////////////////////////////////////////////////////////
+std::list<count_character> Huffman::countCharacters (const std::string& text)
+{
+    std::list <count_character> list_of_characters;
+    try
+    {
+        for(const char& character : text)
+        {
+
+            auto exists_it = std::find_if(list_of_characters.begin()
+                                      , list_of_characters.end()
+                                      , [&character](const count_character& V)
+            {
+                if(std::get<0>(V) == character)
+                {
+                    return true;
+                }
+                else
+                {
+                   return false;
+                }
+            });
+
+            if(exists_it != list_of_characters.end())
+            {
+                (exists_it->second)++;
+            }
+            else
+            {
+                auto new_pair = std::make_pair<char, uint32_t>('h', 0);
+                list_of_characters.push_back(new_pair);
+            }
+        }
+    }
+    catch(...)
+    {}
+
+    return list_of_characters;
+}
