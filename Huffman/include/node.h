4600b592b8e32c2e1e99f3d803b05e6509966e7b
Petros <morakos32@yahoo.gr>
2020-02-15 19:27:01 +0200

    Moddified the Node constuctors

--- Huffman/include/node.h
+++ Huffman/include/node.h
@@ -7,20 +7,20 @@ namespace DataCompression { namespace Huffman {
 class Node
 {
 public:
-    std::shared_ptr<Node> right;
     std::shared_ptr<Node> left;
+    std::shared_ptr<Node> right;
     char label;
     uint32_t value;
 
     ///////////////////////////////////////////////////////////////////////////////
-	Node( std::shared_ptr<Node> _right
-		, std::shared_ptr<Node> _left
+    Node( std::shared_ptr<Node> _left
+        , std::shared_ptr<Node> _right
         , char _label
         , uint32_t _value);
 
     ///////////////////////////////////////////////////////////////////////////////
-    Node( std::shared_ptr<Node> _right
-        , std::shared_ptr<Node> _left
+    Node( std::shared_ptr<Node> _left
+        , std::shared_ptr<Node> _right
         , uint32_t _value);
 
     ///////////////////////////////////////////////////////////////////////////////
--- Huffman/src/node.cpp
+++ Huffman/src/node.cpp
@@ -3,30 +3,30 @@
 namespace DataCompression { namespace Huffman {
 
 ///////////////////////////////////////////////////////////////////////////////
-Node::Node( std::shared_ptr<Node> _right
-		  , std::shared_ptr<Node> _left
+Node::Node( std::shared_ptr<Node> _left
+          , std::shared_ptr<Node> _right
           , char _label
           , uint32_t _value)
-	: right(_right)
-	, left(_left)
+    : left(_left)
+    , right(_right)
     , label(_label)
     , value(_value)
 {}
 
 ///////////////////////////////////////////////////////////////////////////////
-Node::Node( std::shared_ptr<Node> _right
-          , std::shared_ptr<Node> _left
+Node::Node( std::shared_ptr<Node> _left
+          , std::shared_ptr<Node> _right
           , uint32_t _value)
-    : right(_right)
-    , left(_left)
+    : left(_left)
+    , right(_right)
     , value(_value)
 {}
 
 ///////////////////////////////////////////////////////////////////////////////
 Node::Node( char _label
           , uint32_t _value)
-    : right(nullptr)
-    , left(nullptr)
+    : left(nullptr)
+    , right(nullptr)
     , label(_label)
     , value(_value)
 {}
