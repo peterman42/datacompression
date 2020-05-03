50cc4def05b5c797832a1ec5bf933b5b9da214e6
Petros <morakos32@yahoo.gr>
2020-05-03 21:01:58 +0300

    Added unit tests for the Node class. * Currently, the source file contains unit tests for the `Node::isLeaf` function.

---
+++ Huffman/tests/node_tests.cpp
@@ -0,0 +1,28 @@
+#include <gtest/gtest.h>
+#include "node.h"
+
+using namespace DataCompression::Huffman;
+
+TEST(Node, isLeafReturnsTrue)
+{
+    // Given
+    Node node('g', 55);
+
+    // When
+    auto actual = node.isLeaf();
+
+    // Then
+    EXPECT_TRUE(actual);
+}
+
+TEST(Node, isLeafReturnsFalse)
+{
+    // Given
+    Node node(nullptr,nullptr, 55);
+
+    // When
+    auto actual = node.isLeaf();
+
+    // Then
+    EXPECT_FALSE(actual);
+}
